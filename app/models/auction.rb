904d511ec44d5390f16808028ad6ad8067671b35
Alan Jenkins <alanjenkins1978@gmail.com>
2020-12-28 12:46:53 +0000

    Added start and end dates for Auction model as well as filled out some of the front-end views for auctions

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -5,4 +5,14 @@ class Auction < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :start_amount, numericality: { greater_than: 0 } 
+
+  def has_finished
+    # @todo this needs to change if bids force the auction to stay open longer
+    # than the end_date
+    end_date > Date.now
+  end
+
+  def has_started
+    start_date > Date.now
+  end
 end

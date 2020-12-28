594b8323a0a1468dc33d12bff924d8276e25ff4f
Alan Jenkins <alanjenkins1978@gmail.com>
2020-12-28 11:56:16 +0000

    Adds helper to highlight current navigation link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+   def cp(path)
+    if current_page?(path)
+      "bg-gray-900 text-white"
+    else
+      "text-gray-300 hover:bg-gray-700 hover:text-white"
+    end
+   end
 end

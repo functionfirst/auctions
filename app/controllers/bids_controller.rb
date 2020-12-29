d042a2e4f0f1e07d7b414522c2893e663a6fd700
Alan Jenkins <alanjenkins1978@gmail.com>
2020-12-29 16:30:09 +0000

    Add Bids controller

---
+++ app/controllers/bids_controller.rb
@@ -0,0 +1,20 @@
+class BidsController < ApplicationController
+  def create
+    @auction = Auction.find(params[:auction_id])
+    @bid = @auction.bid.create(bid_params)
+    redirect_to auction_path(@auction)
+  end
+
+  def destroy
+    @auction = Auction.find(params[:auction_id])
+    @bid = @auction.bid.find(params[:id])
+    @bid.destroy
+
+    redirect_to auction_path(@auction)
+  end
+
+  private
+  def bid_params
+    params.require(:bid).permit(:user_id, :value, :auction_id)
+  end
+end

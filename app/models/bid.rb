3a52a52fab0e33b79e85e7b971c6d33dfdba62ac
Alan Jenkins <alanjenkins1978@gmail.com>
2020-12-30 11:38:39 +0000

    Add static increments to use when selecting a Bid value
    Also adds validation for Bid value

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,3 +1,6 @@
 class Bid < ApplicationRecord
-  belongs_to :auction
+  belongs_to :auction, inverse_of: :bids
+  validates :value, numericality: { greater_than: 0 }
+
+  INCREMENTS = [1, 5, 10, 20, 50, 100, 125, 175, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000]
 end

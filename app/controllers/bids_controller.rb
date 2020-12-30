ba5c4c64f2a413d6761634bb967d8df3b1b98574
Alan Jenkins <alanjenkins1978@gmail.com>
2020-12-30 11:37:21 +0000

    DRY method to retrieve auction data

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,19 +1,24 @@
 class BidsController < ApplicationController
+  before_action :get_auction
+
   def create
-    @auction = Auction.find(params[:auction_id])
-    @bid = @auction.bid.create(bid_params)
+    @bid = @auction.bids.create(bid_params)
     redirect_to auction_path(@auction)
   end
 
   def destroy
-    @auction = Auction.find(params[:auction_id])
-    @bid = @auction.bid.find(params[:id])
+    @bid = @auction.bids.find(params[:id])
     @bid.destroy
 
     redirect_to auction_path(@auction)
   end
 
   private
+
+  def get_auction
+    @auction = Auction.find(params[:auction_id])
+  end
+
   def bid_params
     params.require(:bid).permit(:user_id, :value)
   end

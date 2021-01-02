eaa24912a4c82ca75e00f52a7fd57a4d435dacdd
Alan Jenkins <alanjenkins1978@gmail.com>
2021-01-02 14:26:25 +0000

    Add extra toggle methods

--- app/javascript/lib/toggle.js
+++ app/javascript/lib/toggle.js
@@ -2,6 +2,8 @@ const addClass = (item, cssName) => item.classList.add(cssName)
 
 const removeClass = (item, cssName) => item.classList.remove(cssName)
 
+const toggleClass = (item, cssName, add) => add ? addClass(item, cssName) : removeClass(item, cssName)
+
 const show = target => {
   const targets = Array.isArray(target) ? target : [target]
   targets.forEach(function(item) {
@@ -16,9 +18,18 @@ const hide = target => {
   })
 }
 
+const toggle = (target, add) => {
+  const targets = Array.isArray(target) ? target : [target]
+  targets.forEach(function(item) {
+    add ? addClass(item, 'hidden') : removeClass(item, 'hidden')
+  })
+}
+
 export {
   addClass,
   removeClass,
+  toggleClass,
   show,
   hide,
+  toggle
 }

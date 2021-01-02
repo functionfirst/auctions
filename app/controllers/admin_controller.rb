e458faca37c55df3034ffb33b5c87e794c30961e
Alan Jenkins <alanjenkins1978@gmail.com>
2021-01-02 21:37:09 +0000

    Replace authentication check in admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,5 @@
 class AdminController < ApplicationController
   layout 'admin'
 
-  before_action :authorised?
-
-  private
-  def authorised?
-    true
-    # unless current_user.has_role? :admin
-    #   flash[:error] = "You're not authorised to view that page"
-    #   redirect_to root_path
-    # end
-  end
+  before_action :authenticate_user!
 end

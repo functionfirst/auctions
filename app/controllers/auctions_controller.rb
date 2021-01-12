3e5788b18d25c60350ba8c5cf6ea2a7f156deb8e
Alan Jenkins <alanjenkins1978@gmail.com>
2021-01-12 22:46:13 +0000

    Add auth skip action for auctions list and details

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,5 +1,5 @@
 class AuctionsController < ApplicationController
-  # skip_before_action :authenticate_user!, :only => [:index, :show]
+  skip_before_action :authenticate_user!, :only => [:index, :show]
 
   def index
     @auctions = Auction.all

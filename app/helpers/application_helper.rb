976f29ab0225354eb196ae5a8b4e6a4c697a5bac
Alan Jenkins <alanjenkins1978@gmail.com>
2021-02-06 22:31:45 +0000

    Add svg icons and helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
       "text-gray-300 hover:bg-gray-700 hover:text-white"
     end
    end
+
+   def show_svg(path)
+    File.open("app/assets/images/svg/#{path}", "rb") do |file|
+      raw file.read
+    end
+  end
 end
